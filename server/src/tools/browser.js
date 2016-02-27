53f49c944319c8333535aac9f15bd561b7566820
Henrique Lechner <hlechner@gmail.com>
2016-02-27 07:12:47 -0300

    Update browser.js

--- server/src/tools/browser.js
+++ server/src/tools/browser.js
@@ -6,11 +6,21 @@ var	system = require('../utils/system').system,
 		tool = require('../tools/tool').tool,
 
 browser = function () {
-	var executable = {'cygwin': 'cmd', 'windows': 'cmd', 'linux': 'xdg-open', 'freebsd': 'xdg-open'}[system.os] || 'open',
-			self = Object.create(tool, {executable: {value: executable}});
+	var executable = {
+		'linux': 'xdg-open',
+		'freebsd': 'xdg-open',
+		'darwin': 'open',
+		'cygwin': 'cmd',
+		'windows': 'cmd'
+	}[system.os] || 'xdg-open',
+	self = Object.create(tool, {executable: {value: executable}});
 			
 	self.exec = function (url, handler) {
-		var args = {'cygwin': ['/c', 'start ' + url], 'windows': ['/c', 'start ' + url]}[system.os] || [url];
+		var args = {
+			'cygwin': ['/c', 'start ' + url], 
+			'windows': ['/c', 'start ' + url]
+		}[system.os] || [url];
+		
 		tool.exec.call(self, args, handler);
 		return self;
 	};
