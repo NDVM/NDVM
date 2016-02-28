3074be270fbc8374b63aeb2bd73e800ba4fe2d81
Henrique Lechner <hlechner@gmail.com>
2016-02-28 12:33:56 -0300

    Fixed NDVM on new nodejs versions

--- server/src/tools/tool.js
+++ server/src/tools/tool.js
@@ -36,6 +36,34 @@ tool = {
 		return this;
 	},
 	
+	// function developed by Jean Lopes https://github.com/jeanlopes
+	toolCallback : function (that ,code, stdout, silent, handler) {
+		var message;
+		if (code !== 0) {
+			message = ["Tool", "'" + that.executable + "'", "exited with code:", code].join(" ") + ".";
+			// tool failed
+			if (silent === true) {
+				console.log("TOOL - silently failed. " + message);
+				// wrapping up
+				if (handler) {
+					handler(code);
+				}
+			} else {
+				// taking it seriously
+				throw message;
+			}
+		} else if (handler) {
+			if (that.parser) {
+				// returning parsed data
+				that.child = null;
+				handler(code, that.parser.parse(stdout.join('')));
+			} else {
+				// returning string
+				handler(code, stdout.join(''));
+			}
+		}
+	},
+
 	// executes tool in specified mode
 	// - args: command line arguments to be passed to process
 	// - handler: handler to run after execution completed
@@ -52,9 +80,19 @@ tool = {
 		debugMsg(["TOOL - executing:", that.executable, args ? args.join(" ") : ""].join(" "));
 		that.child = $child_process.spawn(that.executable, args);
 
+		// workaround to fix NDVM on new nodejs versions
+		var cmd_var;
+		var cmd_reg = new RegExp( /^(.*?)media JOIN roots USING \(rootid\) WHERE mediaid/ );
+		if (that.executable == 'gzip' || cmd_reg.test( args.join(" ") ))
+		 { cmd_var = true; } else { cmd_var = false; }
+
 		// callback
 		function onData(data) {
 			stdout.push(Buffer.isBuffer(data) ? data.toString(that.binary ? 'binary' : 'utf8') : data);
+
+			if ( cmd_var === true ) {
+				that.toolCallback(that, 0, stdout, silent, handler);
+			}
 		} 
 		
 		// data buffering
@@ -64,33 +102,12 @@ tool = {
 		}
 		
 		// handling tool exit
-		that.child.on('exit', function (code) {
-			var message;
-			if (code !== 0) {
-				message = ["Tool", "'" + that.executable + "'", "exited with code:", code].join(" ") + ".";
-				// tool failed
-				if (silent === true) {
-					console.log("TOOL - silently failed. " + message);
-					// wrapping up
-					if (handler) {
-						handler(code);
-					}
-				} else {
-					// taking it seriously
-					throw message;
-				}
-			} else if (handler) {
-				if (that.parser) {
-					// returning parsed data
-					that.child = null;
-					handler(code, that.parser.parse(stdout.join('')));
-				} else {
-					// returning string
-					handler(code, stdout.join(''));
-				}
-			}
-		});
-		
+		if ( cmd_var === false ) {
+			that.child.on('exit', function (code) {
+				that.toolCallback(that, 0, stdout, silent, handler);
+			});
+		}
+
 		return that;
 	}
 };
