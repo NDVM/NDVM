19d5de084164585d7db4ba73355e98744c0f4efd
hlechner <hlechner@gmail.com>
2017-05-24 09:26:10 -0300

    fixes #26

--- server/utils/messager.js
+++ server/utils/messager.js
@@ -17,10 +17,12 @@ var colorMsg = {
 	'darwin': true
 }[os()] || false;
 
-var debug = function debugMsg(msg, debugmsg = true, stdout = false) {
+var debug = function debugMsg(msg, debugmsg, stdout) {
 	// msg = to be printed on console.log/stdout.write
 	// debugmsg = display [DEBUG]
 	// stdout = stdout.write instead of console.log
+	debugmsg ? debugmsg : true
+	stdout ? stdout : false
 	
 	var debugInfo = {
 		true: '\x1b[1;93m[DEBUG]\x1b[0m'
